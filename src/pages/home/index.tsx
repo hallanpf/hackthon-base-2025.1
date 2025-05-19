7610dfaa205873e647c140d91ce45b08e333ccde
Hallan Pedrosa Ferreira <hallanpf@gmail.com>
2025-05-19 20:01:16 -0300

    Feat: "botão index ok"

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,12 +1,14 @@
+import { Button } from '@mui/material';
 import rick from '../../assets/home-image.png'
+import { Link as RouterLink } from 'react-router-dom';
 const Home = () => {
   return (
     <div style={{display: 'flex', flexDirection:'column', alignItems: 'center', justifyContent: 'center', padding: '40px'}}>
       <h2 style={{width: '480px', color:'#12B0C9', textAlign:'center'}}>Está preparado para navegar no mundo de Rick and Morty?</h2>
       <img src={rick} alt="Rick and Morty" style={{width: '480px', height: 'auto', borderRadius: '10px', marginTop: '30px'}} />
-      <button style={{marginTop: '30px', padding: '20px 40px', backgroundColor: '#12B0C9', color: '#fff', border: 'none', borderRadius: '5px', cursor: 'pointer'}}>
+      <Button component={RouterLink} to="/personagem" style={{marginTop: '30px', padding: '20px 40px', backgroundColor: '#12B0C9', color: '#fff', border: 'none', borderRadius: '5px', cursor: 'pointer'}}>
         Iniciar Aventura
-      </button>
+      </Button>
     </div>
   )
 }
